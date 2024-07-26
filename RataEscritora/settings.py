6c5be6d290c11e4eca082fbc7cef8760dc20d954
Mxttosierrv <mat.abarca@duocuc.cl>
2024-07-26 17:59:46 -0400

    cambios menores

--- RataEscritora/settings.py
+++ RataEscritora/settings.py
@@ -43,8 +43,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Librerias
     'crispy_forms',
     'crispy_bootstrap5',
+
+    # Aplicaciones propias
     'pagina'
 ]
 
@@ -112,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
