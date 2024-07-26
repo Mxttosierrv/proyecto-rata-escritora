8c2114982e5bd81b434aa7f56e713da8ffcf464e
Mxttosierrv <mat.abarca@duocuc.cl>
2024-07-26 14:52:03 -0400

    Implementacion de crispy-forms

--- RataEscritora/settings.py
+++ RataEscritora/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'pagina'
 ]
 
@@ -122,12 +124,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Variables de redirección de login y logout
 LOGIN_REDIRECT_URL = 'inicio'
